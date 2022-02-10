f4521870d65576cf5ef2823786b3ead6c2c52f2b
Bryan alvarez <bryan.alvarez@gmail.com>
2022-02-09 21:34:00 -0800

    connected to db

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log('Hello There')
+import { MikroORM} from '@mikro-orm/core';
+import { __prod__ } from './constants';
+import { Post } from './entities/Post';
+
+const main = async () => {
+    const orm = await MikroORM.init({
+        entities: [Post],
+        dbName: 'reddit-clone',
+        user: 'postgres',
+        password: 'postgres',
+        type: 'postgresql',
+        debug: !__prod__,
+    });
+
+    const post = orm.em.create(Post, { title: 'My first post' });
+    await orm.em.persistAndFlush(post);
+    console.log('sql 2 =---------------------------------')
+    await orm.em.nativeInsert(Post, { title: 'My first post' });
+};
+
+main().catch(err => {
+    console.log(err)
+});
