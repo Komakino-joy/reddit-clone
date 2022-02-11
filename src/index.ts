c8054f63335ab2a866945b314481029ed6c0f851
Bryan alvarez <bryan.alvarez1251@gmail.com>
2022-02-10 21:38:34 -0800

    added graphql endpoint with 2 queries.

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,43 @@
 import { MikroORM} from '@mikro-orm/core';
 import { __prod__ } from './constants';
-import { Post } from './entities/Post';
+// import { Post } from './entities/Post';
 import microConfig from './mikro-orm.config';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
+import { PostResolver } from './resolvers/post';
 
 const main = async () => {
     // Connect to Database
     const orm = await MikroORM.init(microConfig);
     // Run Migrations
-    await orm.getMigrator().up()
+    await orm.getMigrator().up();
     // Run SQL
+    // Create Post
     // const post = orm.em.create(Post, { title: 'My first post' });
     // await orm.em.persistAndFlush(post);
-    const posts = await orm.em.find(Post, {});
-    console.log(posts)
+
+    // Fetch post
+    // const posts = await orm.em.find(Post, {});
+    // console.log(posts)
+
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver, PostResolver],
+            validate: false
+        }),
+        context: () => ({ em: orm.em })
+    });
+
+    // Creating a graphql endpoint
+    apolloServer.applyMiddleware({ app })
+
+    app.listen(4000, () => {
+        console.log('Server started on localhost:6000')
+    });
 };
 
 main().catch(err => {
